02eb5f797916713a5991add320b8946796bad953
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2020-12-05 18:19:59 +0100

    move basic retrieval into lib

---
+++ 3/1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+import glob
+from sklearn import svm
+
+from lib.retrieval import BasicRetrieval
+
+
+class SVM_ImageRetrieval(BasicRetrieval):
+    pass
+
+
+if __name__ == "__main__":
+    print("Hail, Satan!")
